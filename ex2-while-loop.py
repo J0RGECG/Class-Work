9d96c49af9358eeecc3a778d38aa4e4633595d05
J0RGECG <139111548+J0RGECG@users.noreply.github.com>
2023-10-05 12:25:05 -0700

    Add files via upload

---
+++ ex2-while-loop.py
@@ -0,0 +1,9 @@
+
+sum = 0
+number = 1
+while number in range (1,101):
+    print(sum, "+",number,"=", sum + number)
+    sum += 1
+    number +=1
+    print("the sum of the number from 1 to 100 is:",sum)
+
