3a5f739953f3dca8ea6b01fea6644d2bdaa53d78
Jorge Campos <139111548+J0RGECG@users.noreply.github.com>
2023-10-12 09:37:25 -0700

    Add files via upload

---
+++ list-string-lab_ex1.py
@@ -0,0 +1,43 @@
+variable1 = "A Red Fox"
+variable2 = "chased a Blue mouse"
+variable3 = "across a Green meadow"
+print(variable1 , variable2 , variable3)
+
+print("----------------------------------------------------")
+
+variable4 = (variable1 + variable2 + variable3)
+print(variable4)
+
+print("------------------------------------------------------")
+print(variable4)
+print(variable4.lower())
+
+print("---------------------------------------------------------")
+print(variable4)
+print(variable4.upper())
+
+print("---------------------------------------------------------")
+
+print(variable4)
+print(variable4.index("l"))
+
+print("------------------------------------------------------")
+
+i = 18
+j = 23
+
+print(variable4[i : j ])
+
+print("-------------------------------------------------------")
+
+c = len(variable4)
+print(c)
+
+print("--------------------------------------------------------")
+
+print(variable4.isnumeric())
+
+print("----------------------------------------------------------")
+
+c = "123443"
+print(c.isnumeric())
