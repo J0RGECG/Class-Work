3faa42910e9424136d4e04039dacb98f7ac9f404
Jorge Campos <139111548+J0RGECG@users.noreply.github.com>
2023-10-12 17:42:34 -0700

    Add files via upload

---
+++ list-string-lab_ex2.py
@@ -0,0 +1,75 @@
+my_num_list = [3, 6, 4, 83, 2, 7, 2, 17, 4, 9, 0, 44, 2]
+
+print("-----------------------------------------------------")
+
+
+print(my_num_list.index(2))
+
+
+print("-----------------------------------------------------")
+
+
+my_num_list.append(8)
+print(my_num_list)
+
+print("-------------------------------------------------------")
+
+i = 4
+
+
+my_num_list.insert(i , 33)
+print (my_num_list)
+
+print("-----------------------------------------------------")
+
+print(my_num_list.count(2))
+
+print("-----------------------------------------------------")
+
+my_num_list.sort()
+print(my_num_list)
+
+print("--------------------------------------------------------")
+
+y = len(my_num_list)
+print(y)
+
+print("-----------------------------------------------------")
+my_num_list.pop()
+my_num_list.pop()
+my_num_list.pop()
+my_num_list.pop()
+print(my_num_list)
+
+print("-----------------------------------------------------")
+
+z = len(my_num_list)
+print(z)
+
+
+print("--------------------------------------------------------")
+
+
+
+while my_num_list == my_num_list:
+    b = len(my_num_list)
+    print(b)
+    break
+
+
+    
+
+
+ 
+
+
+
+
+    
+
+
+
+
+
+
+
