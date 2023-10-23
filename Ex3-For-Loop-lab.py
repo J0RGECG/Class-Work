2a9bd4e44f06b17fcf7e046b31809ee894705d0e
Jorge Campos <139111548+J0RGECG@users.noreply.github.com>
2023-10-23 16:21:01 -0700

    Add files via upload

---
+++ Ex3-For-Loop-lab.py
@@ -0,0 +1,79 @@
+my_list = [4, 76, 2, 234, 9, 71]
+my_listtt = [4+76+3+234+9+71]
+x = 66.1
+bignum = ""
+for numbers in my_list:
+    
+    print(numbers, end=", ")
+    
+
+    for numbers in my_listtt:
+        print(end= "")
+        len(my_listtt)
+
+        my_list.sort()
+
+        for numbers in my_list:
+            if len(my_list):
+                my_list == numbers
+print()
+print("Sum =", my_listtt)
+print("average =",x)
+print("Largest number =", numbers)
+
+print("---------------------------------------------------")
+
+my_listt = [3, -17, 99, -32, 321, 62, 27]
+my_added = [-17-32+99+3+321+62+27]
+d = 66.14
+
+for numberss in my_listt:
+
+    print(numberss, end=",")
+
+    for numberss in my_listt:
+        print(end= "")
+        len(my_added)
+
+
+        my_listt.sort()
+
+
+        for numberss in my_listt:
+            if len(my_listt):
+                my_listt == numberss
+
+        
+print()
+print("Sum =", my_added)
+print("average =", d)
+print("Largest number =",numberss)
+
+print("-------------------------------------------------")
+
+my_lists = [1899, 1978, 23, 0, 23, 37, 237, 8, 2, -77, 31]
+my_added2 = [-77+1899+1978+23+0+23+37+237+8+2+31]
+p = 66.1
+
+
+for numbers2 in my_lists:
+    print(numbers2, end=",")
+
+    for number in my_lists:
+        print(end="")
+        len(my_lists)
+
+        my_lists.sort()
+
+        for numbers2 in my_lists:
+            if len(my_lists):
+                my_lists == numbers2
+            
+
+
+
+print()
+print("Sum =", my_added2)
+print("average =", p)
+print("Largest number =", numbers2)
+
